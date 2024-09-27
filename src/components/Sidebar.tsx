c6cd26a4757bf191eae972786d985963542a1c41
prashant-webconst <prashant.webconst@gmail.com>
2024-09-27 12:45:38 +0530

    initial commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SidebarComponent = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This will scan all your .js/.jsx/.ts/.tsx files for Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
